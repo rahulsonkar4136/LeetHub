13a7757bfed8b3f1d8d4fc06c531ba72d67fb943
rahulsonkar4136 <91192971+rahulsonkar4136@users.noreply.github.com>
2022-08-11 20:43:13 +0530

    Time: 8 ms (76.96%), Space: 42.7 MB (74.92%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,9 @@
+class Solution {
+        public boolean isIsomorphic(String s, String t) {
+     for (int i = 0; i < s.length(); i++) {
+        if (s.lastIndexOf(s.charAt(i)) != t.lastIndexOf(t.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
+}}
